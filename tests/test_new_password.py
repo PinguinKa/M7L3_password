44b7176dc27f3de12e778448b6e6df95cfd51a20
VaNyA1593 <vankaboiko1593@gmail.com>
2025-03-30 11:01:37 +0100

    добавил тесты

--- tests/test_new_password.py
+++ tests/test_new_password.py
@@ -8,6 +8,19 @@ def test_password_characters():
     for char in password:
         assert char in valid_characters
 
+def test_password_length():
+    """Тест, что длина пароля соответствует заданной"""
+    length = 12
+    password = generate_password(length)
+    assert len(password) == length
+
+def test_password_non_repeating():
+    """Тест, что два сгенерированных подряд пароля различаются"""
+    password1 = generate_password(12)
+    password2 = generate_password(12)
+    assert password1 != password2
+git branch
+
 """
 Допиши еще один тест из предложенных. Или придумай свой.
 Если сможешь написать больше, то будет круто!
